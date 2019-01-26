c4e4b923cde16c1802a116e92fad0c7ad3f2b98f
axel8890 <axel8890@gmail.com>
2019-01-26 13:25:58 +0100

    player controller

---
+++ Assets/Scripts/Axel/PlayerControllerDiscarted.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerDiscarted : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public float jumpSpeed = 0.07f;
+    public float gravity = 8f;
+    public bool grounded;
+    private Vector3 velocity = Vector3.zero;
+   // protected Rigidbody rb;
+    protected CharacterController CharCtrl;
+
+    //private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grounded = true;
+       // rb = GetComponent<Rigidbody>();
+        CharCtrl = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity.x = 0f;
+        Vector3 move = Vector3.zero;
+        grounded = CharCtrl.isGrounded;
+
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            grounded = false;
+            velocity.y = jumpSpeed;
+        }
+        velocity.x = Input.GetAxis("Horizontal") * speed;
+        if (!grounded)
+        {
+            velocity.y -= gravity * Time.deltaTime;
+
+        }
+        CharCtrl.Move(velocity);
+    }
+   /* private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.position.y < this.transform.position.y)
+        {
+            velocity.y = 0f;
+            grounded = true;
+        }
+    }*/
+}
