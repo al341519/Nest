a37338543667b6d9d4645a0a67ac39dda8a92adc
axel8890 <axel8890@gmail.com>
2019-01-26 17:20:27 +0100

    Update OpenCanvas.cs

--- Assets/UI_Nest/OpenCanvas.cs
+++ Assets/UI_Nest/OpenCanvas.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject canvas;
+    GameObject player;
+    bool open;
     // Start is called before the first frame update
     void Start()
     {
-        
+        open = false;
     }
 
     // Update is called once per frame
@@ -15,16 +18,29 @@ public class GameManager : MonoBehaviour
     {
         if (open)
         {
-
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                canvas.SetActive(true);
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape)) {
+                canvas.SetActive(false);
+            }
         }
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.name == "Egg") {
+        if (col.gameObject.name == "Player") {
             open = true;
+            player = col.gameObject;
         }
     }
 
-    private void OnTrigger
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            open = false;
+        }
+    }
 }
