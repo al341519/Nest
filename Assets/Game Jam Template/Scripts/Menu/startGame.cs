f70f5f7024f6bfebf22a4edeb70794528199f56a
yuguui <33630524+yuguui@users.noreply.github.com>
2019-01-27 09:07:44 +0100

    Finish

---
+++ Assets/Game Jam Template/Scripts/Menu/startGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    public GameObject canvas;
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(1);
+        canvas.SetActive(false);
+    }
+}
---
+++ Assets/pinchosController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pinchosController : MonoBehaviour
+{
+    public int damage;
+    private float timeBetweenDmgTick;
+    private float timer;
+    private bool dmg;
+
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dmg)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeBetweenDmgTick)
+            {
+                player.GetComponent<PlayerController>().receiveDamage(damage);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            dmg = true;
+            player = col.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            dmg = false;
+            player = null;
+        }
+    }
+}
