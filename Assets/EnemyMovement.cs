bcd3df97dba2d2ec1ae4e552c22390773c4ed638
axel8890 <axel8890@gmail.com>
2019-01-27 02:56:38 +0100

    disolventes

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Animator anim;
+   // Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        anim.SetBool("IsWalking", false);
+       // anim.SetBool("IsWalking", false);
     }
 
     // Update is called once per frame
